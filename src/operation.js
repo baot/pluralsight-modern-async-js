69d58d68ceda8b4f6c2d15e85bf89ab8e8560bb9
Tran Bao <tran.bao51093@gmail.com>
2016-10-12 20:00:14 +0300

    implement operation & operation test

--- src/operation.js
+++ src/operation.js
@@ -42,3 +42,62 @@ function getForecast(city, callback) {
 
   }, delayms)
 }
+
+function fetchCurrentCity() {
+  const operation = Operation();
+
+  getCurrentCity(operation.nodeCallback);
+
+  return operation;
+}
+
+function fetchWeather(city) {
+  const operation = Operation();
+
+  getWeather(city, operation.nodeCallback);
+
+  return operation;
+}
+
+function fetchForecast(city) {
+  const operation = Operation();
+
+  getForecast(city, operation.nodeCallback);
+
+  return operation;
+}
+
+function Operation() {
+  const operation = {
+    successReactions: [],
+    errorReactions: []
+  };
+
+  operation.onCompletion = function onCompletion(onSuccess, onError) {
+    const noop = function() {};
+
+    operation.successReactions.push(onSuccess || noop);
+    operation.errorReactions.push(onError || noop);
+  };
+  operation.onFailure = function onFailure(onError) {
+    operation.onCompletion(null, onError);
+  };
+
+  operation.fail = function fail(error) {
+    operation.errorReactions.forEach(r => r(error));
+  };
+
+  operation.succeed = function succeed(result) {
+    operation.successReactions.forEach(r => r(result));
+  };
+
+  operation.nodeCallback = (err, data) => {
+    if (err) {
+      operation.fail(err);
+      return;
+    }
+    operation.succeed(data);
+  };
+
+  return operation;
+};
---
+++ src/operation.test.js
@@ -0,0 +1,67 @@
+suite.only("operation test");
+
+function callDone(done) {
+  let counter = 0;
+
+  return {
+    afterNCalls: function (expectedCount) {
+      return function multiDone() {
+        counter++;
+        if (counter >= expectedCount) {
+          // if we go over the count, we'll call done multiple times thus failing the test
+          done();
+        }
+      }
+    },
+    afterTwoCalls: function () {
+      return this.afterNCalls(2);
+    }
+  }
+}
+
+test("fetchCurrentCity pass the callbacks later on", (done) => {
+  // initiate operation
+  const operation = fetchCurrentCity();
+
+  // register callbacks
+  operation.onCompletion(
+    result => done(),
+    error => done(error)
+  );
+});
+
+test("pass multiple callbacks - all of them are called", (done) => {
+  // initiate operation
+  const operation = fetchCurrentCity();
+
+  const multiDone = callDone(done).afterTwoCalls();
+
+  // register callbacks
+  operation.onCompletion(result => multiDone());
+  operation.onCompletion(result => multiDone());
+
+});
+
+test("noop if no success handler passed", (done) => {
+  // initiate operation
+  const operation = fetchCurrentCity();
+
+  // noop should register for success handler
+  operation.onFailure(err => done(err));
+
+  // trigger success to make sure noop registered
+  operation.onCompletion(result => done());
+
+});
+
+test("noop if no error handler passed", (done) => {
+  // initiate operation
+  const operation = fetchWeather();
+
+  // noop should register for error handler
+  operation.onCompletion(result => done(new Error('shouldnt succeed')));
+
+  // trigger failure to make sure noop registered
+  operation.onFailure(err => done());
+
+});
